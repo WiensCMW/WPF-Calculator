41ddcec8b0b4f301440c65213bbdb006316a3b54
cornie <cornie.wiens@vidir.com>
2019-05-15 13:23:26 -0500

    Added click event handlers to the number and operation buttons.
    Also started working on the number and operation click event handlers a bit.

--- WpfCalculator/WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/WpfCalculator/MainWindow.xaml.cs
@@ -61,15 +61,50 @@ namespace WpfCalculator
             labelResults.Content = "0";
         }
 
-        private void Button7_Click(object sender, RoutedEventArgs e)
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
+            if (double.TryParse(labelResults.Content?.ToString(), out lastNumber))
+            {
+                labelResults.Content = "0";
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the value of the clicked button
+            int selectedValue = 0;
+
+            if (sender == button0)
+                selectedValue = 0;
+            else if (sender == button1)
+                selectedValue = 1;
+            else if (sender == button2)
+                selectedValue = 2;
+            else if (sender == button3)
+                selectedValue = 3;
+            else if (sender == button4)
+                selectedValue = 4;
+            else if (sender == button5)
+                selectedValue = 5;
+            else if (sender == button6)
+                selectedValue = 6;
+            else if (sender == button7)
+                selectedValue = 7;
+            else if (sender == button8)
+                selectedValue = 8;
+            else if (sender == button9)
+                selectedValue = 9;
+
+            /*  If label results is 0, we override it again with 0. This ensures that if the results is 0
+                repeatedly pressing 0 won't add additional zeros. */
             if (labelResults.Content?.ToString() == "0")
             {
-                labelResults.Content = "7";
+                labelResults.Content = $"{selectedValue}";
             }
             else
             {
-                labelResults.Content = $"{labelResults.Content}7";
+                // Label results != 0, so we add the new clicked value to the end of the existing value.
+                labelResults.Content = $"{labelResults.Content}{selectedValue}";
             }
         }
     }
